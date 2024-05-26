cd31d7277389b8dc0ab65a6807df11a0a64153b5
Martin Chapman <contact@martinchapman.co.uk>
2024-05-26 19:46:27 +0100

    Update made by Phenoflow

---
+++ python/urea-and-electrolytes-m1-blood---primary.py
@@ -0,0 +1,25 @@
+# S Jill Stocks, Evangelos Kontopantelis, Artur Akbarov, Sarah Rodgers, Anthony J Avery, Darren M Aschroft, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"44h..00","system":"readv2"},{"code":"44J8.00","system":"readv2"},{"code":"44J8.11","system":"readv2"},{"code":"44J..11","system":"readv2"},{"code":"44I1.00","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('urea-and-electrolytes-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["urea-and-electrolytes-m1-blood---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["urea-and-electrolytes-m1-blood---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["urea-and-electrolytes-m1-blood---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
